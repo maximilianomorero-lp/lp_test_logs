7f0f510497d8eb51c35e5e6f2e35ff081e12bcf3
maxmorero <maximiliano.morero@localpayment.com>
2026-03-13 17:24:24 +0100

    test

--- src/test/java/com/localpayment/lp_test_logs/LpTestLogsApplicationTests.java
+++ src/test/java/com/localpayment/lp_test_logs/LpTestLogsApplicationTests.java
@@ -2,9 +2,11 @@ package com.localpayment.lp_test_logs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(excludeAutoConfiguration = SystemMetricsAutoConfiguration.class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = SystemMetricsAutoConfiguration.class)
 class LpTestLogsApplicationTests {
 
 	@Test
